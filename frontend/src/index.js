1b1a77ebead1b5ee0904ea43638e5336bf705979
Nerina <u21537144@tuks.co.za>
2024-11-03 23:27:13 +0200

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,3 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-
-// // src/index.js
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import SplashPage from './pages/SplashPage';
-
-// // Render the SplashPage component in the root div
-// ReactDOM.render(<SplashPage />, document.getElementById('root'));
-
